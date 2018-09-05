ee7dfc2127204c63e33bdd5d45476f4db4bb917a
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-05 23:27:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
 function forLoop(array){
-  var newArr = [];
   for (let i = 0; i<25; i++){
-    if (1===array[i]){
-      console.log (newArr.push( "I am 1 strange loop."))
+    if (i===1){
+      array.push( "I am 1 strange loop.")
     }else{
-      console.log (newArr.push("I am "+ array[i] +" strange loops"))
+     array.push("I am "+ array[i] +" strange loops"))
   }
   }
   return newArr;
